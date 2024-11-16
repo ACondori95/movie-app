d60faadc3fe58a19d465b22dba192940b45cf7fd
Alvaro Condori <condorialvaro95@gmail.com>
2024-11-15 22:46:32 -0300

    scrollTop y DRY

--- src/main.js
+++ src/main.js
@@ -4,11 +4,9 @@ const api = axios.create({
   params: {api_key: API_KEY},
 });
 
-async function getTrendingMoviesPreview() {
-  const {data} = await api("trending/movie/day");
-  const movies = data.results;
-
-  trendingMoviesPreviewList.innerHTML = "";
+// Utils
+function createMovies(movies, container) {
+  container.innerHTML = "";
 
   movies.forEach((movie) => {
     const movieContainer = document.createElement("div");
@@ -23,15 +21,12 @@ async function getTrendingMoviesPreview() {
     );
 
     movieContainer.appendChild(movieImg);
-    trendingMoviesPreviewList.appendChild(movieContainer);
+    container.appendChild(movieContainer);
   });
 }
 
-async function getCategoriesPreview() {
-  const {data} = await api("genre/movie/list");
-  const categories = data.genres;
-
-  categoriesPreviewList.innerHTML = "";
+function createCategories(categories, container) {
+  container.innerHTML = "";
 
   categories.forEach((category) => {
     const categoryContainer = document.createElement("div");
@@ -47,29 +42,28 @@ async function getCategoriesPreview() {
 
     categoryTitle.appendChild(categoryTitleText);
     categoryContainer.appendChild(categoryTitle);
-    categoriesPreviewList.appendChild(categoryContainer);
+    container.appendChild(categoryContainer);
   });
+}
+
+// Llamados a la API
+async function getTrendingMoviesPreview() {
+  const {data} = await api("trending/movie/day");
+  const movies = data.results;
+
+  createMovies(movies, trendingMoviesPreviewList);
+}
+
+async function getCategoriesPreview() {
+  const {data} = await api("genre/movie/list");
+  const categories = data.genres;
+
+  createCategories(categories, categoriesPreviewList);
 }
 
 async function getMoviesByCategory(id) {
   const {data} = await api("discover/movie", {params: {with_genres: id}});
   const movies = data.results;
 
-  genericSection.innerHTML = "";
-
-  movies.forEach((movie) => {
-    const movieContainer = document.createElement("div");
-    movieContainer.classList.add("movie-container");
-
-    const movieImg = document.createElement("img");
-    movieImg.classList.add("movie-img");
-    movieImg.setAttribute("alt", movie.title);
-    movieImg.setAttribute(
-      "src",
-      `https://image.tmdb.org/t/p/w300/${movie.poster_path}`
-    );
-
-    movieContainer.appendChild(movieImg);
-    genericSection.appendChild(movieContainer);
-  });
+  createMovies(movies, genericSection);
 }
